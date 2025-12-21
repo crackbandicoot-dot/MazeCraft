1e34836a87948c36be7697ae8df972e220b38c51
Kevin Torres <kevintorresperera@gmail.com>
2025-12-21 17:54:03 -0500

    feat : Improve solveMaze using seq as queue

---
+++ dist-newstyle/build/x86_64-linux/ghc-9.6.7/MazeCraft-0.1.0.0/x/MazeCraft/build/MazeCraft/autogen/Paths_MazeCraft.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+#if __GLASGOW_HASKELL__ >= 810
+{-# OPTIONS_GHC -Wno-prepositive-qualified-module #-}
+#endif
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -w #-}
+module Paths_MazeCraft (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+
+import qualified Control.Exception as Exception
+import qualified Data.List as List
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir `joinFileName` name)
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+
+
+
+
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+bindir     = "/home/kevin/.cabal/bin"
+libdir     = "/home/kevin/.cabal/lib/x86_64-linux-ghc-9.6.7/MazeCraft-0.1.0.0-inplace-MazeCraft"
+dynlibdir  = "/home/kevin/.cabal/lib/x86_64-linux-ghc-9.6.7"
+datadir    = "/home/kevin/.cabal/share/x86_64-linux-ghc-9.6.7/MazeCraft-0.1.0.0"
+libexecdir = "/home/kevin/.cabal/libexec/x86_64-linux-ghc-9.6.7/MazeCraft-0.1.0.0"
+sysconfdir = "/home/kevin/.cabal/etc"
+
+getBinDir     = catchIO (getEnv "MazeCraft_bindir")     (\_ -> return bindir)
+getLibDir     = catchIO (getEnv "MazeCraft_libdir")     (\_ -> return libdir)
+getDynLibDir  = catchIO (getEnv "MazeCraft_dynlibdir")  (\_ -> return dynlibdir)
+getDataDir    = catchIO (getEnv "MazeCraft_datadir")    (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "MazeCraft_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "MazeCraft_sysconfdir") (\_ -> return sysconfdir)
+
+
+
+joinFileName :: String -> String -> FilePath
+joinFileName ""  fname = fname
+joinFileName "." fname = fname
+joinFileName dir ""    = dir
+joinFileName dir fname
+  | isPathSeparator (List.last dir) = dir ++ fname
+  | otherwise                       = dir ++ pathSeparator : fname
+
+pathSeparator :: Char
+pathSeparator = '/'
+
+isPathSeparator :: Char -> Bool
+isPathSeparator c = c == '/'
--- src/MazeSolver.hs
+++ src/MazeSolver.hs
@@ -1,31 +1,64 @@
 module MazeSolver (solveMaze) where
 
 import DataStructures (Position, Path, Maze, GeneratedMaze(..))
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import qualified Data.Sequence as Seq
+import Data.List (foldl')
 
--- Solve a maze given a start position
--- Returns the path from start to end, or empty list if no solution
 -- ENTRY POINT DO NOT DELETE
+-- Solve the maze using BFS. Uses:
+--  - `Seq` as a queue (O(1) push/pop),
+--  - `Set` for visited checks (O(log n) or better),
+--  - `Map` to store parent pointers for path reconstruction.
 solveMaze :: GeneratedMaze -> Path
-solveMaze (GeneratedMaze s e g) = bfs [[s]] []
+solveMaze (GeneratedMaze s e g) =
+    let parents = bfs (Seq.singleton s) (Set.singleton s) Map.empty
+    in reconstructPath s e parents
   where
-    bfs [] _ = []  -- No solution
-    bfs (path:paths) visited
-        | current == e = reverse path  -- Found the end
-        | current `elem` visited = bfs paths visited  -- Already visited
-        | otherwise = bfs (paths ++ newPaths) (current:visited)
-      where
-        current = head path
-        neighbors = filter (isValid g) (getNeighbors current)
-        newPaths = [n:path | n <- neighbors, n `notElem` visited]
+    bfs :: Seq.Seq Position -> Set.Set Position -> Map.Map Position Position -> Map.Map Position Position
+    bfs Seq.Empty _ parents = parents
+    bfs (cur Seq.:<| q) visited parents
+        | cur == e = parents
+        | otherwise =
+            let neighbors = filter (isValid g) (getNeighbors cur)
+                -- Only consider neighbors not yet visited
+                unvisited = filter (`Set.notMember` visited) neighbors
+                -- Mark them visited immediately to avoid duplicates in the queue
+                visited' = foldl' (flip Set.insert) visited unvisited
+                -- Enqueue unvisited neighbors
+                q' = q Seq.>< Seq.fromList unvisited
+                -- Record parent pointers: parent[neighbor] = cur
+                parents' = foldl' (\m v -> Map.insert v cur m) parents unvisited
+            in bfs q' visited' parents'
 
--- Get adjacent positions (up, down, left, right)
+    -- Walk parents from goal back to start, building the path in forward order.
+    reconstructPath :: Position -> Position -> Map.Map Position Position -> Path
+    reconstructPath src dst parents
+        | src == dst = [src]
+        | otherwise = case go dst [] of
+            Nothing -> []
+            Just p -> p
+      where
+        go :: Position -> [Position] -> Maybe Path
+        go cur acc
+            | cur == src = Just (src : acc)
+            | otherwise = case Map.lookup cur parents of
+                Nothing -> Nothing
+                Just p -> go p (cur : acc)
+
+-- Neighbors (left, right, up, down)
 getNeighbors :: Position -> [Position]
 getNeighbors (x,y) = [(x-1,y), (x+1,y), (x,y-1), (x,y+1)]
 
--- Check if a position is valid and walkable
+-- Bounds and wall check.
 isValid :: Maze -> Position -> Bool
-isValid maze (x,y)
-    | y < 0 || y >= length maze = False
-    | x < 0 || x >= length (head maze) = False
-    | otherwise = maze !! y !! x /= '#'
+isValid maze (x,y) =
+    let rows = length maze
+    in if rows == 0 then False
+       else
+           let cols = length (head maze)
+           in if y < 0 || y >= rows || x < 0 || x >= cols
+              then False
+              else (maze !! y !! x) /= '#'
 
